a20bf25611ccfdd83ad0a1b86734902056d5d05c
amoghmmurgodmath <amoghmmurgodmath@gmail.com>
2025-03-20 11:03:27 +0000

    "cart"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,24 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
- 
-
-    return (
-        <div>
-            <nav>
-                <div className="site-name">Better Buys</div>                
-                <div className="nav-links">
-                    <Link to="/">Home</Link>
-                    <Link to="/add-product">Add Product</Link>
-                    <Link to="/cart">Cart</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/contact">Contact</Link>
-                </div>
-                <div className="auth-links">
-                    
-                    <Link to="/signup">Sign Up</Link>
-                </div>
-                
-            </nav>
-            </div>
-    );
+  return (
+    <div>
+      <nav>
+        <div className="site-name">Better Buys</div>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/add-product">Add Product</Link>
+          <Link to="/cart" className="navbar-cart-btn">Cart</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <div className="auth-links">
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
+
 export default Navbar;
