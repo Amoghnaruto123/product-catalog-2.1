893a1faf0b35d09745d4e0c389502760c4383b9e
amoghmmurgodmath <amoghmmurgodmath@gmail.com>
2025-03-20 14:51:17 +0000

    "icon"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
   return (
     <div>
       <nav>
         <div className="site-name">Better Buys</div>
+
         <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/add-product">Add Product</Link>
-          <Link to="/cart" className="navbar-cart-btn">Cart</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/add-product">Add Product</NavLink>
+          <NavLink to="/cart" className="navbar-cart-btn">
+            <FontAwesomeIcon icon={faCartShopping} style={{ fontSize: '25px' }}/>
+          </NavLink>
+          <NavLink to="/about">About</NavLink>
         </div>
         <div className="auth-links">
           <NavLink to="/signup" className="navbar-signup">Sign Up</NavLink>
